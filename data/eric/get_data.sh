dc0ba22c732a948aaaaa60b41cef1a83dcad5e0e
Jon Ball <91580027+j-a-ball@users.noreply.github.com>
2023-10-10 10:43:37 -0700

    Update get_data.sh
    implement source search to increase number of records

--- data/eric/get_data.sh
+++ data/eric/get_data.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # sociology of education records
-curl -X GET "https://api.ies.ed.gov/eric/?search=issn%3A%22ISSN-0038-0407%22&format=xml&rows=2000" -H "accept: */*" > se.xml
+curl -X GET "https://api.ies.ed.gov/eric/?search=source%3A%22sociology%20of%20education%22%20AND%20NOT%20source%3A%22international%22%20AND%20NOT%20source%3A%22british%22%20AND%20NOT%20source%3A%22Leisure%22&format=xml&rows=2000" -H "accept: */*" > se.xml
 # american journal of education records
-curl -X GET "https://api.ies.ed.gov/eric/?search=issn%3A%22ISSN-0195-6744%22&format=xml&rows=2000" -H "accept: */*" > aje.xml
+curl -X GET "https://api.ies.ed.gov/eric/?search=source%3A%22american%20journal%20of%20education%22%20%20AND%20NOT%20source%3A%22health%22%20AND%20NOT%20source%3A%22business%22%20AND%20NOT%20source%3A%22pharmaceutical%22%20AND%20NOT%20source%3A%22distance%22%20AND%20NOT%20source%3A%22sexuality%22%20AND%20NOT%20source%3A%22engineering%22&format=xml&rows=2000" -H "accept: */*" > aje.xml
 # american educational research journal records
-curl -X GET "https://api.ies.ed.gov/eric/?search=issn%3A%22ISSN-0002-8312%22&format=xml&rows=2000" -H "accept: */*" > aerj.xml
+curl -X GET "https://api.ies.ed.gov/eric/?search=source%3A%22american%20educational%20research%20journal%22&format=xml&rows=2000" -H "accept: */*" > aerj.xml
 # british journal of sociology of education records
-curl -X GET "https://api.ies.ed.gov/eric/?search=issn%3A%22ISSN-0142-5692%22&format=xml&rows=2000" -H "accept: */*" > bjse.xml
+curl -X GET "https://api.ies.ed.gov/eric/?search=source%3A%22british%20journal%20of%20sociology%20of%20education%22&format=xml&rows=2000" -H "accept: */*" > bjse.xml
 # international studies in sociology of education
-curl -X GET "https://api.ies.ed.gov/eric/?search=issn%3A%22ISSN-0962-0214%22&format=xml&rows=2000" -H "accept: */*" > isse.xml
+curl -X GET "https://api.ies.ed.gov/eric/?search=source%3A%22international%20studies%20in%20sociology%20of%20education%22&format=xml&rows=2000" -H "accept: */*" > isse.xml
+# american sociological review
+curl -X GET "https://api.ies.ed.gov/eric/?search=source%3A%22American%20Sociological%20Review%22&format=xml&rows=2000" -H "accept: */*" > asr.xml
+# american journal of sociology
+curl -X GET "https://api.ies.ed.gov/eric/?search=source%3A%22american%20journal%20of%20sociology%22%20AND%20NOT%20source%3A%22economics%22&format=xml&rows=2000" -H "accept: */*" > ajs.xml
