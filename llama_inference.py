e45b60d8c9a7c1f7ea211918ee10f2427d79214f
JBall <jonball@stanford.edu>
2023-10-11 21:57:46 -0700

    shorten context window to fit 2-pg examples

--- llama_inference.py
+++ llama_inference.py
@@ -8,28 +8,24 @@ import os
 def main(args):
     # inputs are .txt, outputs are .json {"input":str, "output":str}
     # load an initial one-shot example
-    with open(args.example_json, "r") as infile:
+    with open(args.example_vars, "r") as infile:
         exampleVars = json.load(infile)
     # jinja template loader
-    jinjaLoader = jinjitsu(args.template_dir, args.template_jinja, exampleVars)
+    jinjaLoader = jinjitsu(args.template_dir, args.template_file, exampleVars)
     # load filepaths to loop over
-    for dirpath, dirnames, filenames in os.walk(args.txt_dir):
-        files = [file for file in filenames if file.endswith(".txt")]
+    for dirpath, dirnames, filenames in os.walk(args.input_dir):
+        files = sorted([file for file in filenames if file.endswith(".txt")])
     # Generate completions for each article
+    pgnum = 2
     completions = []
     for file in tqdm(files):
-        with open(os.path.join(args.txt_dir, file), "r") as infile:
+        with open(os.path.join(args.input_dir, file), "r") as infile:
             pgs = infile.read().split("<newpage>")
-        # Render template with output blank
-        templateVars = {
-            "input": """
-            <newpage json_schema={"quantitative": bool, "qualitative": bool, "primary/secondary": bool, "tertiary": bool, "inequality": bool, "nonstructural": bool, "culture": bool, "school": bool, "state": bool, "labor": bool, "comparative": bool, "methods": bool}>
-            """.join(pgs[:3]),
-            "output": ""}
-        prompt = "\n".join(
-            [jinjaLoader.example, jinjaLoader.render( templateVars )])
+        # Render template with first n pages and output blank
+        templateVars = {"input": "\n".join(pgs[:pgnum]), "output": ""}
+        prompt = "\n".join([jinjaLoader.example, jinjaLoader.render( templateVars )])
         # Get GPT-4 completion and overwrite output
-        completion = subprocess.run(["../codellama/llama.cpp/main", "-m", "../codellama/llama.cpp/models/7B/code-base-Q5_K.bin", "-c", "8000", "-n", "80", "-t", "0", "--multiline_input", "-p", prompt], capture_output=True)
+        completion = subprocess.run(["../codellama/llama.cpp/main", "-m", "../codellama/llama.cpp/models/7B/code-base-Q5_K.bin", "-c", "3324", "-n", "80", "-t", "0", "--multiline_input", "-p", prompt], capture_output=True)
         completion = completion.stdout.decode("utf-8")
         # Get only the completion following the prompt
         completion = completion.replace(prompt, "")
@@ -55,11 +51,11 @@ class jinjitsu():
 if __name__ == "__main__":
     # args
     parser = argparse.ArgumentParser()
-    parser.add_argument("--txt_dir", type=str, default="data/txts/SE")
-    parser.add_argument("--completion_dir", type=str, default="completions/SE")
+    parser.add_argument("--input_dir", type=str, default="data/txts/SE")
+    parser.add_argument("--completion_dir", type=str, default="completions/SE/7B_Q5")
     parser.add_argument("--template_dir", type=str, default="prompts")
-    parser.add_argument("--template_jinja", type=str, default="txt.jinja")
-    parser.add_argument("--example_json", type=str, default="prompts/examples/txt_example.json")
+    parser.add_argument("--template_file", type=str, default="txt.jinja")
+    parser.add_argument("--example_vars", type=str, default="prompts/examples/txt_example.json")
     args = parser.parse_args()
     # run
     main(args)
